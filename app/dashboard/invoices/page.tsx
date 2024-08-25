7d0d97e5376ce41695dc4abf3012c088262aece7
Justin Weathersby <justinweathersby@gmail.com>
2024-08-25 19:03:11 -0400

    Add pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
+
 import Pagination from "@ui/invoices/pagination";
 import Search from "@ui/search";
 import Table from "@ui/invoices/table";
 import { CreateInvoice } from "@ui/invoices/buttons";
 import { lusitana } from "@ui/fonts";
 import { InvoicesTableSkeleton } from "@ui/skeletons";
-import { Suspense } from "react";
+
+import { fetchInvoicesPages } from "@lib/data";
 
 export default async function Page({
   searchParams,
@@ -16,6 +19,8 @@ export default async function Page({
 }) {
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -30,7 +35,7 @@ export default async function Page({
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
