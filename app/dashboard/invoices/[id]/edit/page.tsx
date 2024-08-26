06b456d55bf50c5ffa85e0f6c753d02728b6b337
Justin Weathersby <justinweathersby@gmail.com>
2024-08-26 10:44:59 -0400

    Add error page and not found page

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,12 +3,18 @@ import Breadcrumbs from "@ui/invoices/breadcrumbs";
 
 import { fetchInvoiceById, fetchCustomers } from "@lib/data";
 
+import { notFound } from "next/navigation";
+
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
